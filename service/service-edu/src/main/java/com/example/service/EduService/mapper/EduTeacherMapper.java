8bd14e73243b7d3a8465a504176c7e6a653b4456
ianscodeworld <ianscodeworld@gmail.com>
2022-07-24 23:30:48 +0800

    inintial commit

---
+++ service/service-edu/src/main/java/com/example/service/EduService/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.example.service.EduService.mapper;
+
+import com.example.service.EduService.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-07-24
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ service/service-edu/src/main/java/com/example/service/EduService/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.service.EduService.service.impl;
+
+import com.example.service.EduService.entity.EduTeacher;
+import com.example.service.EduService.mapper.EduTeacherMapper;
+import com.example.service.EduService.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-07-24
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
